4fe1912023c8929a975550c1bf81b78e7ce00a29
Melvin <Melvine@melvins-mbp.attlocal.net>
2014-12-25 15:22:52 -0800

    included minutes
    changed to include minute updates from just seconds

--- Stopwatch/ViewController.swift
+++ Stopwatch/ViewController.swift
@@ -12,7 +12,8 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var timer: UILabel!
     var clickLimit = 0      //prevents misuse of playButton
-    var count = 1
+    var seconds = 1
+    var minutes = 0
     var time: NSTimer!
 
     override func viewDidLoad() {
@@ -29,8 +30,8 @@ class ViewController: UIViewController {
     
     @IBAction func restartButton(sender: AnyObject) {
         time.invalidate()
-        count = 1
-        timer.text = "0"
+        seconds = 1
+        timer.text = "00:00"
         clickLimit = 0
     }
     @IBAction func pauseButton(sender: AnyObject) {
@@ -39,9 +40,31 @@ class ViewController: UIViewController {
     }
 
     func clock(){
-        println(count)
-        timer.text = String(count)
-        count++
+        println(seconds)
+        
+        if(seconds == 60){
+            seconds = 0
+            ++minutes
+        }
+        
+        if(seconds < 10){
+            if(minutes < 10){
+                timer.text = "0\(minutes):" + "0" + String(seconds);
+            }
+            else{
+                timer.text = "\(minutes):" + "0" + String(seconds);
+            }
+        }
+        else{
+            if(minutes < 10){
+                timer.text = "0\(minutes):" + String(seconds);
+            }
+            else{
+                timer.text = "\(minutes):" + String(seconds);
+            }
+        }
+        
+        seconds++
     }
     
     override func didReceiveMemoryWarning() {
